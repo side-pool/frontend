09004912d6f0c3ae65ce4e74609eab57c53a8211
dididy <dev.yongjaelee@gmail.com>
2021-10-10 01:18:41 +0900

    env: custom-types를 정의한다

---
+++ @types/custom-types/index.d.ts
@@ -0,0 +1,6 @@
+declare module 'custom-types' {
+  export namespace customTypes {
+    type ElementSize = 'xxs' | 'xs' | 'sm' | 'md' | 'lg' | 'xl' | 'xxl';
+    type ElementColor = 'black' | 'gray' | 'blue' | 'red';
+  }
+}
