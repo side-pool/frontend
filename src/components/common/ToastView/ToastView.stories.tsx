4c4276c204f689356f3421612b803f471947fc45
dididy <dev.yongjaelee@gmail.com>
2021-10-10 01:24:12 +0900

    feat: ToastView 컴포넌트를 추가한다

---
+++ src/components/common/ToastView/ToastView.stories.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Story, Meta } from '@storybook/react';
+import ToastView, { ToastProps } from '@src/components/common/ToastView';
+
+export default {
+  title: 'Toast View',
+  component: ToastView,
+} as Meta;
+
+const Template: Story<ToastProps> = (props) => <ToastView {...props} />;
+export const toastView = Template.bind({});
+
+toastView.args = {
+  message: 'message',
+};
