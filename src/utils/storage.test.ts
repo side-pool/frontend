0889a656935e8a7e27debe97cade996543f58bfb
SeungYoung Oh <seung-00@naver.com>
2021-10-11 20:38:04 +0900

    test: storage 유틸함수 테스트 코드를 수정한다

--- src/utils/storage.test.ts
+++ src/utils/storage.test.ts
@@ -1,39 +1,74 @@
 import { saveItem, loadItem, removeItem } from './storage';
 
+const localStorageMock = (() => {
+  let store: Record<string, string> = {};
+
+  return {
+    getItem(key: string) {
+      return store[key];
+    },
+    setItem(key: string, value: string | number) {
+      store[key] = value.toString();
+    },
+    removeItem(key: string) {
+      delete store[key];
+    },
+    clear() {
+      store = {};
+    },
+  };
+})();
+
+Object.defineProperty(window, 'sessionStorage', {
+  value: localStorageMock,
+});
+
 describe('storage', () => {
-  jest.spyOn(Object.getPrototypeOf(window.sessionStorage), 'setItem');
+  const testData = { userId: 1, userEmail: 'example@gmail.com' };
 
   beforeEach(() => {
-    Object.getPrototypeOf(window.sessionStorage).setItem = jest.fn();
-    Object.getPrototypeOf(window.sessionStorage).getItem = jest.fn();
-    Object.getPrototypeOf(window.sessionStorage).removeItem = jest.fn();
+    window.sessionStorage.clear();
+    jest.restoreAllMocks();
   });
 
-  describe('saveItem', () => {
-    it('calls sessionStorage setItem', () => {
-      saveItem('key', 'value');
+  it('setItem 으로 저장한 값은 loadItem 으로 가져와진다', () => {
+    const getItemSpy = jest.spyOn(window.sessionStorage, 'getItem');
+    const setItemSpy = jest.spyOn(window.sessionStorage, 'setItem');
 
-      expect(sessionStorage.setItem).toBeCalledWith('key', 'value');
+    saveItem('userInfo', JSON.stringify(testData));
+    const loadedItem = loadItem('userInfo') ?? '';
+
+    expect(setItemSpy).toBeCalled();
+    expect(getItemSpy).toBeCalledWith('userInfo');
+    expect(JSON.parse(loadedItem)).toEqual({
+      userId: 1,
+      userEmail: 'example@gmail.com',
     });
   });
 
-  describe('loadItem', () => {
-    it('calls sessionStorage getItem', () => {
-      loadItem('key');
+  it('저장된 값이 없다면 loadItem 의 리턴 값은 undefined이다', () => {
+    const getItemSpy = jest.spyOn(window.sessionStorage, 'getItem');
+    const loadedItem = loadItem('userInfo');
 
-      expect(sessionStorage.getItem).toBeCalledWith('key');
-    });
+    expect(loadedItem).toBeUndefined();
+    expect(getItemSpy).toBeCalledWith('userInfo');
   });
 
-  describe('removeItem', () => {
-    it('calls sessionStorage removeItem', () => {
-      loadItem('key');
+  it('saveItem으로 저장된 값은 removeItem으로 지울 수 있다', () => {
+    const getItemSpy = jest.spyOn(window.sessionStorage, 'getItem');
+    const setItemSpy = jest.spyOn(window.sessionStorage, 'setItem');
+    const removeItemSpy = jest.spyOn(window.sessionStorage, 'removeItem');
 
-      expect(sessionStorage.getItem).toBeCalledWith('key');
+    saveItem(
+      'userInfo',
+      JSON.stringify({ userId: 1, userEmail: 'example@gmail.com' }),
+    );
+    removeItem('userInfo');
+    const loadedItem = loadItem('userInfo');
 
-      removeItem('key');
-
-      expect(sessionStorage.getItem).toBeCalledWith('key');
-    });
+    expect(setItemSpy).toBeCalled();
+    expect(removeItemSpy).toBeCalled();
+    expect(getItemSpy).toBeCalledWith('userInfo');
+    expect(loadedItem).toBeUndefined();
   });
 });
