805d3f38ab7429276bd53015d03f392d94e1bb39
dididy <dev.yongjaelee@gmail.com>
2021-10-11 19:30:22 +0900

    refactor: 예지님 디자인대로 Modal 컴포넌트를 변경한다

--- src/components/common/Modal/Modal.test.tsx
+++ src/components/common/Modal/Modal.test.tsx
@@ -23,34 +23,17 @@ describe('<Modal />', () => {
   });
 
   context('props를 넘기는 상황에서', () => {
-    it('closeModal props를 넘기면 컴포넌트에 반영된다', () => {
-      const { container } = renderModal({
-        closeModal: () => console.log('something'),
-      });
-
-      expect(
-        getByTestId(container, 'modal').childNodes[0].childNodes[0],
-      ).toHaveClass('close-button');
-    });
-    it('closeModal props를 넘기면 컴포넌트에 반영된다', () => {
+    it('fadeout props를 넘기면 컴포넌트에 반영된다', () => {
       const { container } = renderModal({ fadeout: true });
 
       expect(getByTestId(container, 'modal')).toHaveClass('fadeout');
-    });
-    it('className props를 넘기면 컴포넌트에 반영된다', () => {
-      const className = 'className';
-      const { container } = renderModal({ className });
-
-      expect(getByTestId(container, 'modal').childNodes[0]).toHaveClass(
-        className,
-      );
     });
     it('headerText props를 넘기면 컴포넌트에 반영된다', () => {
       const { container } = renderModal({ headerText: 'something' });
 
       expect(
         getByTestId(container, 'modal').childNodes[0].childNodes[0],
-      ).toHaveClass('modal-header');
+      ).toHaveClass('modalHeader');
     });
     it('footer props를 넘기면 컴포넌트에 반영된다', () => {
       const { container } = renderModal({
@@ -60,13 +43,6 @@ describe('<Modal />', () => {
       expect(
         getByTestId(container, 'modal').childNodes[0].childNodes[1],
       ).toHaveClass('footer');
-    });
-    it('width props를 넘기면 컴포넌트에 반영된다', () => {
-      const { container } = renderModal({ width: 50 });
-
-      expect(getByTestId(container, 'modal').childNodes[0]).toHaveStyle(
-        'width: 50px;',
-      );
     });
   });
 });
