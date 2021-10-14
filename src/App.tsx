2f1420b985fc68f6f68b6eb2026c4d0ce2783d99
SeungYoung Oh <seung-00@naver.com>
2021-10-14 14:19:08 +0900

    feat: <JoinPage /> 를 마크업한다

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Route, Switch } from 'react-router-dom';
 
 import LandingPage from '@src/pages/LandingPage';
 import LoginPage from '@src/pages/LoginPage';
+import JoinPage from './pages/JoinPage';
 
 import Sidebar from '@src/components/common/Sidebar';
 import styles from '@src/App.module.scss';
@@ -17,6 +18,7 @@ const App = () => {
         <div className={styles.content}>
           <Route exact path="/" component={LandingPage} />
           <Route path="/login" component={LoginPage} />
+          <Route path="/join" component={JoinPage} />
         </div>
       </div>
     </Switch>
---
+++ src/pages/JoinPage/index.tsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import Typography from '@src/components/common/Typography';
+import styles from './JoinPage.module.scss';
+import Card from '@src/components/common/Card';
+import Input from '@src/components/common/Input';
+import Button from '@src/components/common/Button';
+
+const JoinPage = () => {
+  return (
+    <div className={styles.JoinPage}>
+      <Typography
+        fontSize="xxl"
+        fontWeight="bold"
+        role="heading"
+        className="title"
+      >
+        회원가입
+      </Typography>
+      <Card className={styles.wideCard}>
+        <div className={styles.topArea}>
+          <div className={styles.infoArea}>
+            <dl className={styles.infoList}>
+              <dt>
+                <Typography fontSize="md" fontWeight="medium">
+                  Username
+                </Typography>
+              </dt>
+              <dd>
+                <Input placeholder="username" maxWidth={true} />
+              </dd>
+            </dl>
+            <dl className={styles.infoList}>
+              <dt>
+                <Typography fontSize="md" fontWeight="medium">
+                  Password
+                </Typography>
+              </dt>
+              <dd>
+                <Input placeholder="password" maxWidth={true} />
+              </dd>
+            </dl>
+            <dl className={styles.infoList}>
+              <dt>
+                <Typography fontSize="md" fontWeight="medium">
+                  Nickname
+                </Typography>
+              </dt>
+              <dd>
+                <Input placeholder="nickname" maxWidth={true} />
+              </dd>
+            </dl>
+          </div>
+          <Button className={styles.checkButton} primary={true}>
+            중복확인
+          </Button>
+        </div>
+        <Button className={styles.JoinButton} primary={true}>
+          회원가입
+        </Button>
+      </Card>
+    </div>
+  );
+};
+
+export default JoinPage;
