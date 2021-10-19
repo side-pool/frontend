ee4549ed993ecf0ca33dfd4d98c5f96dac15a3a7
SeungYoung Oh <seung-00@naver.com>
2021-10-20 03:55:29 +0900

    feat: status code 분기 처리를 위한 상수들을 추가한다

---
+++ src/utils/common.ts
@@ -0,0 +1,22 @@
+import { HttpStatusCode } from '@src/constant/enums';
+import { AxiosError } from 'axios';
+
+/**
+ *
+ * 일반적인 에러들에 대한 문구를 제공한다
+ * @param error
+ * @returns {string}
+ */
+export const getErrorText = (error: AxiosError<unknown>) => {
+  const NOT_FOUND_TEXT = '페이지를 찾을 수 없습니다 😅';
+  const REST_ERROR_TEXT = '알 수 없는 오류가 발생했습니다 😭';
+
+  switch (error.response?.status) {
+    case HttpStatusCode.NOT_FOUND:
+      return NOT_FOUND_TEXT;
+    case HttpStatusCode.BAD_REQUEST:
+    case HttpStatusCode.UNAUTHORIZED:
+    default:
+      return REST_ERROR_TEXT;
+  }
+};
