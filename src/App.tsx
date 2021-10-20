d3d4c72b7a3102c57716bc587efe7e546a9dc307
dididy <dev.yongjaelee@gmail.com>
2021-10-20 23:40:21 +0900

    fix: 공통 알림 모달을 App 컴포넌트에 추가한다

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,21 @@ import { Route, Switch } from 'react-router-dom';
 
 import { QueryClientProvider, QueryClient } from 'react-query';
 
+import styles from '@src/App.module.scss';
+
 import LandingPage from '@src/pages/LandingPage';
 import LoginPage from '@src/pages/LoginPage';
 import JoinPage from '@src/pages/JoinPage';
 import IdeaPage from '@src/pages/IdeaPage';
 
 import Sidebar from '@src/components/common/Sidebar';
-import styles from '@src/App.module.scss';
+
+import Modal from '@src/components/common/Modal';
+import Button from '@src/components/common/Button';
+import Typography from '@src/components/common/Typography';
+
+import { useAppDispatch, useUiState, hideAlertModal } from '@src/store';
+
 import { getApiInstance } from '@src/utils/context';
 
 const queryClient = new QueryClient({
@@ -27,10 +35,36 @@ const queryClient = new QueryClient({
 });
 
 const App = () => {
+  const dispatch = useAppDispatch();
+  const { alertModalIsVisible, alertModalTitle, alertModalContent } =
+    useUiState();
+
   return (
     <QueryClientProvider client={queryClient}>
       <Switch>
         <div className={styles.App}>
+          <Modal
+            closeModal={() => dispatch(hideAlertModal())}
+            headerText={alertModalTitle}
+            footer={{
+              submitButton: (
+                <Button primary onClick={() => dispatch(hideAlertModal())}>
+                  확인
+                </Button>
+              ),
+            }}
+            isVisible={alertModalIsVisible || false}
+          >
+            <Typography
+              fontSize="xs"
+              fontWeight="regular"
+              textColor="black"
+              textAlign="center"
+            >
+              {alertModalContent}
+            </Typography>
+          </Modal>
+
           <div className={styles.sidebar}>
             <Sidebar />
           </div>
