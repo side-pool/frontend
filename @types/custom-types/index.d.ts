a1459a822d5e998d6a12e8536fbf32eaef39e7c1
dididy <dev.yongjaelee@gmail.com>
2021-10-23 16:55:40 +0900

    env: svg 파일에 대한 타입관련 설정을 추가한다

--- @types/custom-types/index.d.ts
+++ @types/custom-types/index.d.ts
@@ -4,3 +4,11 @@ declare module 'custom-types' {
     type ElementColor = 'black' | 'gray' | 'blue' | 'red';
   }
 }
+
+declare module '*.svg' {
+  import React = require('react');
+
+  export const ReactComponent: React.SFC<React.SVGProps<SVGSVGElement>>;
+  const src: string;
+  export default src;
+}
