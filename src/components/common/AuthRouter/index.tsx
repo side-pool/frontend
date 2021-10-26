ab720035f87c3637d5e163a168f176972aa13955
dididy <dev.yongjaelee@gmail.com>
2021-10-27 00:46:14 +0900

    feat: AuthRouter를 정의한다

---
+++ src/components/common/AuthRouter/index.tsx
@@ -0,0 +1,36 @@
+import React, { JSXElementConstructor } from 'react';
+import { Route, Redirect, RouteProps } from 'react-router-dom';
+import { useGetUser } from '@src/hooks/useUserQuery';
+
+interface AuthRouteProps extends RouteProps {
+  component: JSXElementConstructor<any>;
+}
+
+const AuthRoute = ({
+  component: Component,
+  render,
+  ...rest
+}: AuthRouteProps) => {
+  const { data, isLoading } = useGetUser();
+
+  return (
+    <Route
+      {...rest}
+      render={(props) =>
+        isLoading || data ? (
+          render ? (
+            render(props)
+          ) : (
+            <Component {...props} />
+          )
+        ) : (
+          <Redirect
+            to={{ pathname: '/login', state: { from: props.location } }}
+          />
+        )
+      }
+    />
+  );
+};
+
+export default AuthRoute;
