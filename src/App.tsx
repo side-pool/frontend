8bc0e91273b995262531ec5b04749486aa01dd5e
SeungYoung Oh <seung-00@naver.com>
2021-10-31 06:50:51 +0900

    feat: <AuthRoute /> 를 적용한다

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,42 @@
 import React from 'react';
 import { Route, Switch, useLocation } from 'react-router-dom';
-
-import { QueryClientProvider, QueryClient } from 'react-query';
-
 import styles from '@src/App.module.scss';
-
-import AuthRoute from '@src/components/common/AuthRouter';
-
 import LandingPage from '@src/pages/LandingPage';
 import LoginPage from '@src/pages/LoginPage';
 import JoinPage from '@src/pages/JoinPage';
 import IdeaPage from '@src/pages/IdeaPage';
-
 import Sidebar from '@src/components/common/Sidebar';
-
-import { getApiInstance } from '@src/utils/context';
-
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      queryFn: async ({ queryKey: [url] }) => {
-        if (typeof url === 'string') {
-          const { data } = await getApiInstance().get(url);
-          return data;
-        }
-        throw new Error('Invalid QueryKey');
-      },
-    },
-  },
-});
+import { useCheckAuth } from '@src/hooks/useUserQuery';
+import AuthRoute from '@src/components/common/AuthRouter';
 
 const App = () => {
   const { pathname } = useLocation();
+  const { isSuccess } = useCheckAuth();
 
   return (
-    <QueryClientProvider client={queryClient}>
-      <Switch>
-        <div className={styles.App}>
-          <div className={styles.sidebar}>
-            <Sidebar pathname={pathname} />
-          </div>
-          <div className={styles.content}>
-            <Route exact path="/" component={LandingPage} />
-            <Route path="/login" component={LoginPage} />
-            <Route path="/join" component={JoinPage} />
-            <AuthRoute path="/idea" component={IdeaPage} />
-          </div>
+    <Switch>
+      <div className={styles.App}>
+        <div className={styles.sidebar}>
+          <Sidebar pathname={pathname} />
         </div>
-      </Switch>
-    </QueryClientProvider>
+        <div className={styles.content}>
+          <Route exact path="/" component={LandingPage} />
+          <AuthRoute
+            path="/login"
+            component={LoginPage}
+            isAuth={!isSuccess}
+            redirectPath="/"
+          />
+          <AuthRoute
+            path="/join"
+            component={JoinPage}
+            isAuth={!isSuccess}
+            redirectPath="/"
+          />
+          <Route path="/idea" component={IdeaPage} />
+        </div>
+      </div>
+    </Switch>
   );
 };
 
