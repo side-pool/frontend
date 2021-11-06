3f68b0a1be91b568e509f1c8e9a149a8c937ea01
dididy <dev.yongjaelee@gmail.com>
2021-11-06 23:07:54 +0900

    feat: Idea 페이지에 Search 및 Filter를 연결한다

--- src/components/Idea/IdeaCardContainer/index.tsx
+++ src/components/Idea/IdeaCardContainer/index.tsx
@@ -3,9 +3,11 @@ import IdeaCard from '@src/components/Idea/IdeaCard';
 import styles from './IdeaCardContainer.module.scss';
 import { useReadIdeas } from '@src/hooks/useIdeaQuery';
 import Spinner from '@src/components/common/Spinner';
+import { useIdeaState } from '@src/store';
 
 const IdeaCardContainer = () => {
-  const { data: dataArr, isSuccess, isLoading } = useReadIdeas();
+  const idea = useIdeaState();
+  const { data: dataArr, isSuccess, isLoading } = useReadIdeas(idea);
 
   return (
     <div className={styles.IdeaCardContainer}>
