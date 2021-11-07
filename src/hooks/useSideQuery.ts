cf06f6b348f5a599ed363497936da00afee556ac
dididy <dev.yongjaelee@gmail.com>
2021-11-08 01:08:19 +0900

    fix: useCreateSlice 커스텀 훅을 추가한다

--- src/hooks/useSideQuery.ts
+++ src/hooks/useSideQuery.ts
@@ -1,6 +1,30 @@
-import { useQuery } from 'react-query';
+import { useMutation, useQuery } from 'react-query';
 import { AxiosError } from 'axios';
 import { ReadSidesData, SideParams } from '@src/models';
+import { getApiInstance } from '@src/utils/context';
+
+export type CreateSideParams = {
+  categoryNames: string[];
+  detail: string;
+  githubIdentifier: number;
+  githubLink: string;
+  logoUrl: string;
+  organizationIds: number[];
+  pushedAt: string;
+  recruiting: boolean;
+  serviceLink: string;
+  skillIds: number[];
+  summary: string;
+  title: string;
+};
 
 export const useReadSides = (params: SideParams) =>
   useQuery<ReadSidesData, AxiosError<unknown>>(['/sides', params]);
+
+export const useCreateSide = () => {
+  return useMutation<string, AxiosError<{ error: string }>, CreateSideParams>(
+    async (params) => {
+      return await getApiInstance().post('/sides', { ...params });
+    },
+  );
+};
