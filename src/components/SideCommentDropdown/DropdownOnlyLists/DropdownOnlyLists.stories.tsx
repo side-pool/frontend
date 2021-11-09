521a3aa6132660f0d783d8f20b1e41176072d20f
dididy <dev.yongjaelee@gmail.com>
2021-11-10 00:12:57 +0900

    feat: DropdownOnlyLists 컴포넌트 및 스토리를 추가한다

---
+++ src/components/SideCommentDropdown/DropdownOnlyLists/DropdownOnlyLists.stories.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import { Story, Meta } from '@storybook/react';
+import DropdownOnlyLists, {
+  DropdownOnlyListsProps,
+} from '@src/components/SideCommentDropdown/DropdownOnlyLists';
+import { COMMENT_DROPDOWN } from '@src/components/SideCommentDropdown';
+
+export default {
+  title: 'components/Dropdown Only Lists',
+  component: DropdownOnlyLists,
+} as Meta;
+
+const Template: Story<DropdownOnlyListsProps> = () => {
+  console.log(COMMENT_DROPDOWN);
+  return <DropdownOnlyLists dropdwonLists={COMMENT_DROPDOWN} />;
+};
+
+export const dropdownOnlyLists = Template.bind({});
---
+++ src/components/SideCommentDropdown/DropdownOnlyLists/index.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+
+import Typography from '@src/components/common/Typography';
+import styles from './DropdownOnlyLists.module.scss';
+import { setSide, useAppDispatch } from '@src/store';
+
+export interface DropdownOnlyListsProps {
+  dropdwonLists: string[];
+}
+
+const DropdownOnlyLists = ({ dropdwonLists }: DropdownOnlyListsProps) => {
+  const dispatch = useAppDispatch();
+  return (
+    <div className={styles.DropdownOnlyLists}>
+      {dropdwonLists?.length === 0 ? (
+        <div className={styles.eachList}>
+          <div className={styles.checkboxContainer}>
+            <Typography fontSize="xs" lineHeight="wide">
+              선택할 목록이 없습니다.
+            </Typography>
+          </div>
+        </div>
+      ) : (
+        dropdwonLists?.map((each, index) => (
+          <div
+            key={each}
+            className={styles.eachList}
+            data-index={index}
+            onClick={() => dispatch(setSide({ commentTag: index }))}
+            aria-hidden
+          >
+            <label className={styles.checkboxContainer}>
+              <Typography fontSize="xs" lineHeight="wide">
+                {each}
+              </Typography>
+            </label>
+          </div>
+        ))
+      )}
+    </div>
+  );
+};
+
+export default DropdownOnlyLists;
