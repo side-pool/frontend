e5a3a7b8572a1d9a028d12d6c45c8cabafe8758f
SeungYoung Oh <seung-00@naver.com>
2021-11-12 02:55:10 +0900

    feat: 무한 스크롤을 아이디어 페이지 적용한다

--- src/components/Idea/IdeaCardContainer/index.tsx
+++ src/components/Idea/IdeaCardContainer/index.tsx
@@ -1,19 +1,47 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import IdeaCard from '@src/components/Idea/IdeaCard';
 import styles from './IdeaCardContainer.module.scss';
 import { useReadIdeas } from '@src/hooks/useIdeaQuery';
 import Spinner from '@src/components/common/Spinner';
 import { useIdeaState } from '@src/store';
+import useIntersectionObserver from '@src/hooks/useIntersectionObserver';
+import useThrottle from '@src/hooks/useThrottle';
 
 const IdeaCardContainer = () => {
+  const target = useRef<HTMLDivElement | null>(null);
   const idea = useIdeaState();
-  const { data: dataArr, isSuccess, isLoading } = useReadIdeas(idea);
+  const {
+    data: infiniteData,
+    fetchNextPage,
+    isSuccess,
+    isLoading,
+    isFetchingNextPage,
+    isFetchedAfterMount,
+  } = useReadIdeas(idea);
+
+  const handleInfiniteFetch = useThrottle(() => {
+    fetchNextPage();
+  }, 100);
+
+  useIntersectionObserver({
+    target,
+    onIntersect: (entries: IntersectionObserverEntry[]) => {
+      entries.forEach((entry) => {
+        if (isFetchedAfterMount && entry.isIntersecting) {
+          handleInfiniteFetch();
+        }
+      });
+    },
+  });
 
   return (
     <div className={styles.IdeaCardContainer}>
-      {isLoading && <Spinner />}
       {isSuccess &&
-        dataArr?.map((data) => <IdeaCard key={data.id} idea={data} />)}
+        infiniteData?.pages?.map((page) =>
+          page.map((data) => <IdeaCard key={data.id} idea={data} />),
+        )}
+      {(isFetchingNextPage || isLoading) && <Spinner />}
+      <div ref={target} className="last-item" />
     </div>
   );
 };
--- src/hooks/useIdeaQuery.ts
+++ src/hooks/useIdeaQuery.ts
@@ -1,14 +1,37 @@
+import { useInfiniteQuery, useMutation } from 'react-query';
 import { Idea, ReadIdeasData } from '@src/models';
 import { IdeaParams } from '@src/store/ideaSlice';
 import { getApiInstance } from '@src/utils/context';
-import { AxiosError } from 'axios';
-import { useMutation, useQuery } from 'react-query';
 
 type CreateUpdateIdeaParam = Pick<Idea, 'title' | 'content'> &
   Pick<Partial<Idea>, 'hashtags'>;
 
-export const useReadIdeas = (params: IdeaParams) =>
-  useQuery<ReadIdeasData, AxiosError<unknown>>(['/ideas', params]);
+export const useReadIdeas = (params: IdeaParams) => {
+  const PAGE_SIZE = 5;
+  let page = 0;
+
+  return useInfiniteQuery(
+    ['/ideas', params] as const,
+    async ({ queryKey: [url, params], pageParam = 0 }) => {
+      const { data } = await getApiInstance().get<ReadIdeasData>(url, {
+        params: {
+          ...params,
+          isDone: false,
+          page: pageParam,
+          size: PAGE_SIZE,
+        },
+      });
+      // TODO: 마지막 페이지 체크
+      return data;
+    },
+    {
+      getNextPageParam: () => {
+        page += 1;
+        return page;
+      },
+    },
+  );
+};
 
 export const useCreateIdea = () => {
   return useMutation<string, unknown, CreateUpdateIdeaParam>(async (params) => {
