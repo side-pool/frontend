57ae5b39f02925b98bbe90c4fb87c6a453fc948b
SeungYoung Oh <seung-00@naver.com>
2021-11-12 01:58:48 +0900

    feat: useThrottle 을 추가한다

---
+++ src/hooks/useThrottle.ts
@@ -0,0 +1,18 @@
+import { useRef } from 'react';
+
+const useThrottle = <T>(callback: (...args: T[]) => void, delay: number) => {
+  const timer = useRef<ReturnType<typeof setTimeout> | null>(null);
+
+  return (...args: T[]) => {
+    if (timer.current) return;
+
+    timer.current = setTimeout(() => {
+      timer.current = null;
+      console.log('done');
+    }, delay);
+
+    callback(...args);
+  };
+};
+
+export default useThrottle;
