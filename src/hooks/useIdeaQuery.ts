b73c330e1f529cdf5c05e3c850774a1bd7f5d06d
dididy <dev.yongjaelee@gmail.com>
2021-11-12 17:32:12 +0900

    fix: 머지하면서 생긴 이슈를 해결한다

--- src/hooks/useIdeaQuery.ts
+++ src/hooks/useIdeaQuery.ts
@@ -1,4 +1,4 @@
-import { useInfiniteQuery, useMutation } from 'react-query';
+import { useInfiniteQuery, useMutation, useQuery } from 'react-query';
 import { Idea, ReadIdeasData } from '@src/models';
 import { IdeaParams } from '@src/store/ideaSlice';
 import { getApiInstance } from '@src/utils/context';
@@ -26,7 +26,7 @@ export const useReadIdeas = (params: IdeaParams) => {
 };
 
 export const useReadIdea = (id: number) =>
-  useQuery<Idea, AxiosError<unknown>>([`/ideas/${id}`]);
+  useQuery<Idea, unknown>([`/ideas/${id}`]);
 
 export const useCreateIdea = () => {
   return useMutation<string, unknown, CreateUpdateIdeaParam>(async (params) => {
