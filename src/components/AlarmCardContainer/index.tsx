cbe153e1edb52380f47fdd33c7b7cca5a95a6065
dididy <dev.yongjaelee@gmail.com>
2021-11-14 00:26:16 +0900

    fix: 불필요한 useMemo 를 삭제한다

--- src/components/AlarmCardContainer/index.tsx
+++ src/components/AlarmCardContainer/index.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo } from 'react';
+import React from 'react';
 import { useHistory } from 'react-router-dom';
 
 import { useDeleteAlarm, useTurnToReadAlarm } from '@src/hooks/useMyPageQuery';
