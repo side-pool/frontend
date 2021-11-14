6922df04eba8f1c7ec61a3c5f1643cb654495570
SeungYoung Oh <seung-00@naver.com>
2021-11-14 20:41:36 +0900

    feat: useGetHashTags 기능을 수정한다

--- src/hooks/useHashTagQuery.ts
+++ src/hooks/useHashTagQuery.ts
@@ -1,9 +1,31 @@
 import { useQuery } from 'react-query';
-import { AxiosError } from 'axios';
 import { HashTagInfo } from '@src/models';
+import { getApiInstance } from '@src/utils/context';
+
+const HASH_CIRCLE_NUM = 10;
 
 export const useGetHashTags = () =>
-  useQuery<HashTagInfo[], AxiosError<unknown>>(['/hashtags'], {
-    refetchOnWindowFocus: false,
-    staleTime: Infinity,
-  });
+  useQuery(
+    ['/hashtags'],
+    async ({ queryKey: [url] }) => {
+      if (typeof url === 'string') {
+        const { data: responseData } = await getApiInstance().get<
+          HashTagInfo[]
+        >(url);
+
+        if (responseData.length < HASH_CIRCLE_NUM) {
+          return responseData;
+        }
+
+        const data = responseData
+          .sort(() => 0.5 - Math.random())
+          .slice(0, HASH_CIRCLE_NUM);
+
+        return data;
+      }
+    },
+    {
+      refetchOnWindowFocus: false,
+      staleTime: Infinity,
+    },
+  );
