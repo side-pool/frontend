968e2111c84916c756472c4773dcdfdead651353
SeungYoung Oh <seung-00@naver.com>
2021-11-14 20:47:40 +0900

    feat: <HashTagCircle /> 로직을 추가한다

--- src/components/HashTagBanner/HashTagCircle.tsx
+++ src/components/HashTagBanner/HashTagCircle.tsx
@@ -2,10 +2,23 @@ import React from 'react';
 import styles from './HashTagCircle.module.scss';
 
 const BLUE_ACTIVE = '#004ce5';
-const DEFAULT_RAD = 15;
+const DEFAULT_RADIUS = 15;
+const MAX_RADIUS = 150;
+
+const getRadius = (count: number, weight: number) => {
+  const rad = count * weight * DEFAULT_RADIUS;
+  if (rad > MAX_RADIUS) {
+    return MAX_RADIUS;
+  }
+  if (rad < DEFAULT_RADIUS) {
+    return DEFAULT_RADIUS;
+  }
+  return rad;
+};
 
 export interface HashTagCircleProps {
   count: number;
+  weight?: number;
   opacity: number;
   word: string;
   coordinate?: {
@@ -16,6 +29,7 @@ export interface HashTagCircleProps {
 
 const HashTagCircle = ({
   count,
+  weight = 1,
   opacity,
   word,
   coordinate,
@@ -25,7 +39,7 @@ const HashTagCircle = ({
       <circle
         cx={coordinate?.x ?? '50%'}
         cy={coordinate?.y ?? '50%'}
-        r={`${count + DEFAULT_RAD}`}
+        r={`${getRadius(count, weight)}`}
         opacity={`${opacity}`}
       />
       <text
