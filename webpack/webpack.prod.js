d942edae49b021b16e50df49b3a1bd260815a4c1
dididy <dev.yongjaelee@gmail.com>
2021-11-20 23:51:39 +0900

    hotfix: 파비콘 문제를 해결한다

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -41,7 +41,7 @@ module.exports = merge(baseConfig, {
   plugins: [
     new HtmlWebPackPlugin({
       template: path.resolve(TEMPLATE_PATH, 'index.html'),
-      favicon: path.resolve(TEMPLATE_PATH, 'favicon.ico'),
+      favicon: 'favicon.ico',
       minify: true,
     }),
     new MiniCssExtractPlugin({
