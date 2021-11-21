0c8dee68d964441cd520ce628006fb4d466a7a76
dididy <dev.yongjaelee@gmail.com>
2021-11-22 04:17:25 +0900

    feat: Gnb show, hide를 위한 useScrollPosition 커스텀 훅을 추가한다

---
+++ src/hooks/useScrollPosition.ts
@@ -0,0 +1,44 @@
+import { useState, useEffect } from 'react';
+import useThrottle from './useThrottle';
+
+const useScrollPosition = (): { isActive: boolean } => {
+  const [data, setData] = useState({
+    x: 0,
+    y: 0,
+    lastX: 0,
+    lastY: 0,
+  });
+
+  const [isActive, setIsActive] = useState(true);
+
+  const handleScroll = useThrottle(() => {
+    setData((last) => {
+      return {
+        x: window.scrollX,
+        y: window.scrollY,
+        lastX: last.x,
+        lastY: last.y,
+      };
+    });
+  }, 500);
+
+  useEffect(() => {
+    handleScroll();
+    window.addEventListener('scroll', () => handleScroll());
+
+    return () => {
+      window.removeEventListener('scroll', () => handleScroll());
+    };
+  }, []);
+
+  useEffect(() => {
+    let flag = false;
+    if (data.y > 150 && data.y - data.lastY > 0) flag = true;
+
+    setIsActive(flag);
+  }, [data.y, data.lastY]);
+
+  return { isActive };
+};
+
+export default useScrollPosition;
