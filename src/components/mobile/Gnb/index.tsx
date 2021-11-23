0f7166431b1e9b9b63e3b407c6c5452dd982ad0c
dididy <dev.yongjaelee@gmail.com>
2021-11-23 22:37:12 +0900

    fix: 상세보기 페이지에서 Gnb를 네비게이션 바 형태로 변경되도록 처리한다

--- src/components/mobile/Gnb/index.tsx
+++ src/components/mobile/Gnb/index.tsx
@@ -5,6 +5,7 @@ import useScrollPosition from '@src/hooks/useScrollPosition';
 import Typography from '@src/components/common/Typography';
 import SearchMini from '@src/assets/SearchMini.svg';
 import Create from '@src/assets/Create.svg';
+import ArrowLeft from '@src/assets/ArrowLeft.svg';
 
 import Input, { ParentRef } from '@src/components/common/Input';
 import {
@@ -15,13 +16,17 @@ import {
   setIdea,
 } from '@src/store';
 import { useAuth } from '@src/hooks/useUserQuery';
+import { useHistory } from 'react-router';
+import Button from '@src/components/common/Button';
 
 interface GnbProps {
   pathname: string;
   showGithubModal: () => void;
+  showIdeaForm: () => void;
 }
 
-const Gnb = ({ pathname, showGithubModal }: GnbProps) => {
+const Gnb = ({ pathname, showGithubModal, showIdeaForm }: GnbProps) => {
+  const history = useHistory();
   const { search: sideSearch } = useSideState();
   const { search: ideaSearch } = useIdeaState();
   const { data: isAuth } = useAuth();
@@ -34,6 +39,37 @@ const Gnb = ({ pathname, showGithubModal }: GnbProps) => {
   const searchRef = useRef({} as ParentRef);
 
   const isSide = useMemo(() => pathname.includes('side'), [pathname]);
+
+  const isNavigation = useMemo(
+    () => pathname.split('').filter((each) => each === '/').length === 2,
+    [pathname],
+  );
+
+  if (isNavigation) {
+    return (
+      <div
+        ref={wrapperRef}
+        className={cn(styles.Gnb, isActive && styles.isActiveGnb)}
+      >
+        <div className="gnbButtonArea">
+          <ArrowLeft onClick={() => history.push(isSide ? '/side' : '/idea')} />
+        </div>
+        <Typography fontSize="lg" fontWeight="bold">
+          {isSide ? '사이드' : '아이디어'}
+        </Typography>
+        <div className="gnbButtonArea">
+          {isAuth && (
+            <Button variant="text">
+              <Typography fontSize="md" textColor="gray">
+                등록
+              </Typography>
+            </Button>
+          )}
+        </div>
+      </div>
+    );
+  }
+
   return (
     <div
       ref={wrapperRef}
@@ -79,7 +115,9 @@ const Gnb = ({ pathname, showGithubModal }: GnbProps) => {
 
       <div className={styles.gnbIconContainer}>
         {isAuth && !isSearchOpen && (
-          <Create onClick={() => showGithubModal()} />
+          <Create
+            onClick={() => (isSide ? showGithubModal() : showIdeaForm())}
+          />
         )}
         <SearchMini
           onClick={() => {
