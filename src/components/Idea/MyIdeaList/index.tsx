c743d97f37c3a733887f7181a3f7460ea6070ddb
dididy <dev.yongjaelee@gmail.com>
2021-11-27 01:18:41 +0900

    fix: MyIdeaList의 breakpoint 기준을 수정한다

--- src/components/Idea/MyIdeaList/index.tsx
+++ src/components/Idea/MyIdeaList/index.tsx
@@ -8,10 +8,8 @@ import { useReadMyIdea } from '@src/hooks/useMyPageQuery';
 import Typography from '@src/components/common/Typography';
 
 const BREAKPOINT_COLS = {
-  default: 4,
-  1800: 3,
-  1500: 2,
-  1100: 1,
+  default: 2,
+  1500: 1,
 };
 
 const MyIdeaList = () => {
